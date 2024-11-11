e19a55413cf4af561a8244bda3a5e51ea577981e
copperminesp <son5121502@gmail.com>
2024-11-11 17:44:57 +0900

    feat: 스피너 선택 후 적용 시 텍스트 변화

--- Chapter10_4/app/src/main/java/corp/cloudint/spinnerexample2/MainActivity.java
+++ Chapter10_4/app/src/main/java/corp/cloudint/spinnerexample2/MainActivity.java
@@ -1,6 +1,9 @@
 package corp.cloudint.spinnerexample2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    final String[] cars = {"아이오닉 5N", "아반떼 N", "BWM M2", "BMW M4", "BMW M5"};
+    Spinner spinner;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner = findViewById(R.id.spinner);
+        text = findViewById(R.id.text);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, cars);
+        spinner.setAdapter(adapter);
+        findViewById(R.id.btn).setOnClickListener(v -> setText());
+    }
+
+    private void setText(){
+        text.setText("선택 항목: " + spinner.getSelectedItem().toString());
     }
 }
